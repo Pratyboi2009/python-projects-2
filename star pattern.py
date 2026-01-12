80bc5bfdb0ded3459edfef79ce483f52c38015c7
Pratyboi2009 <pratyushkrsahoo2009@gmail.com>
2026-01-12 23:10:03 +0530

    added star pattern problems class x

---
+++ star pattern.py
@@ -0,0 +1,35 @@
+'''
+  *
+ ***
+*****
+...
+
+'''
+n = int(input("enter number to make a star pattern :"))
+for i in range(n+1):
+    print(" "*(n-i),end ="")
+    print("*"*(2*i-1),end ="")
+    print("\n")
+
+'''
+* * *
+*   * 
+* * *
+'''
+# n = int(input("enter number to make a star pattern :"))
+# print("*"*n)
+
+# for i in range(n-2):
+#     print("*"+" "*(n-2)+"*")
+    
+# print("*"*n)
+
+'''
+*
+**
+***
+'''
+# n = int(input("enter number to make a star pattern :"))
+# for i in range(n+1):
+#     print("*"*i)
+    
